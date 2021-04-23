0b02d06515dfd1ee43e0b9edf17db15a2b1e7da9
eclipseop <mh@otterdev.com>
2021-04-23 12:59:35 -0400

    hol lotta title shit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
 import Entry from "../components/Entry";
+import Head from "next/head";
 
 const index: React.FC = () => {
 	const [url, setUrl] = useState("");
 
 	return (
-		<div className="min-h-screen bg-pink-300">
-			<div className="flex flex-col">
-				<h1 className="text-4xl mx-auto text-white mb-1">
-					Alice333.ai
-				</h1>
-				<Entry url={url} onChange={setUrl} />
+		<div>
+			<Head>
+				<title>Alice333.ai</title>
+				<meta name="og:title" content="Alice333.ai" key="title"></meta>
+			</Head>
+			<div className="min-h-screen bg-pink-300">
+				<div className="flex flex-col">
+					<h1 className="text-4xl mx-auto text-white mb-1">
+						Alice333.ai
+					</h1>
+					<Entry url={url} onChange={setUrl} />
+				</div>
 			</div>
 		</div>
 	);
