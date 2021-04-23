5bd95cf6bcd119c02ac30a752b22d808a11ef3c0
eclipseop <mh@otterdev.com>
2021-04-23 19:04:13 -0400

    increment clicks, closes #6

--- pages/api/[uid].tsx
+++ pages/api/[uid].tsx
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Pool } from "pg";
-import { nanoid } from "nanoid";
 
 const connectionString =
 	"postgresql://db:wkbk5qd6fzk78kwm@app-f6cdc796-9f88-4f91-ae7f-412f02bebaa1-do-user-7621143-0.b.db.ondigitalocean.com:25060/db?sslmode=require";
@@ -19,6 +18,9 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 		]);
 		const url = data.rows[0].url;
 
+		client.query("UPDATE urls SET clicks = clicks + 1 WHERE UID = ($1)", [
+			req.query.uid,
+		]);
 		res.redirect(url);
 	} catch {
 		res.status(404).send("");
