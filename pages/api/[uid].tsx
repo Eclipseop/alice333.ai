9a78f0d3d1f6e551c318e5842c561b73bdfdfd8a
eclipseop <mh@otterdev.com>
2021-04-23 19:20:31 -0400

    text upload

--- pages/api/[uid].tsx
+++ pages/api/[uid].tsx
@@ -5,6 +5,10 @@ const connectionString =
 	"postgresql://db:wkbk5qd6fzk78kwm@app-f6cdc796-9f88-4f91-ae7f-412f02bebaa1-do-user-7621143-0.b.db.ondigitalocean.com:25060/db?sslmode=require";
 const pool = new Pool({ connectionString });
 
+const regex = new RegExp(
+	/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi
+);
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	if (req.method !== "GET") {
 		res.status(400).send("Invalid HTTP Methods");
@@ -16,12 +20,17 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 		const data = await client.query("SELECT * from URLS where UID = ($1)", [
 			req.query.uid,
 		]);
-		const url = data.rows[0].url;
+		const content = data.rows[0].url;
 
 		client.query("UPDATE urls SET clicks = clicks + 1 WHERE UID = ($1)", [
 			req.query.uid,
 		]);
-		res.redirect(url);
+
+		if (!content.match(regex)) {
+			res.status(300).send(content);
+			return;
+		}
+		res.redirect(content);
 	} catch {
 		res.status(404).send("");
 	} finally {
--- pages/api/url.tsx
+++ pages/api/url.tsx
@@ -6,10 +6,6 @@ const connectionString =
 	"postgresql://db:wkbk5qd6fzk78kwm@app-f6cdc796-9f88-4f91-ae7f-412f02bebaa1-do-user-7621143-0.b.db.ondigitalocean.com:25060/db?sslmode=require";
 const pool = new Pool({ connectionString });
 
-const regex = new RegExp(
-	/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi
-);
-
 const handlePost = async (req: NextApiRequest, res: NextApiResponse) => {
 	const client = await pool.connect();
 	try {
@@ -17,10 +13,6 @@ const handlePost = async (req: NextApiRequest, res: NextApiResponse) => {
 		const url = req.query.url;
 
 		if (typeof url != "string") {
-			return;
-		}
-		if (!url.match(regex)) {
-			res.status(400).send("Not a valid URL.");
 			return;
 		}
 
