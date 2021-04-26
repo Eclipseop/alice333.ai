0258698a52d35d4d22a1cc82e26e308f20faa30e
eclipseop <mh@otterdev.com>
2021-04-26 08:19:13 -0700

    more socials

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -12,9 +12,14 @@ const Socials: React.FC<Props> = ({ visible, setVis }: Props) => (
         <div className="absolute bottom-0 right-0 mb-28 mr-10 flex z-10 flex-col bg-white rounded max-w-min p-2">
             <p className="font-semibold mx-auto">Alice333.ai</p>
             <div className="flex gap-1">
-                <a href="http://alice333.ai/api/patreon">Patreon</a>
-                <a href="http://alice333.ai/api/youtube">YouTube</a>
-                <a href="http://alice333.ai/api/reddit">Reddit</a>
+                <a href="https://alice333.ai/api/patreon">Patreon</a>
+                <a href="https://alice333.ai/api/youtube">YouTube</a>
+                <a href="https://alice333.ai/api/reddit">Reddit</a>
+            </div>
+            <div className="flex gap-1">
+                <a href="https://alice333.ai/api/discord">Discord</a>
+                <a href="https://alice333.ai/api/instagram">Instagram</a>
+                <a href="https://alice333.ai/api/alice333">Creator</a>
             </div>
 
             <button
