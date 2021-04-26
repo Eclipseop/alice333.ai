f8c1250c86e365899ab036ddea506cc8cee5ad12
eclipseop <mh@otterdev.com>
2021-04-26 09:00:25 -0700

    styling on mobile!!!

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable no-use-before-define */
 import React from 'react';
 
@@ -10,16 +11,16 @@ interface Props {
 const Socials: React.FC<Props> = ({ visible, setVis }: Props) => (
     <div className={visible ? 'visible' : 'invisible'}>
         <div className="absolute bottom-0 right-0 mb-28 mr-10 flex z-10 flex-col bg-white rounded max-w-min p-2">
-            <p className="font-semibold mx-auto">Alice333.ai</p>
-            <div className="flex gap-1">
-                <a target="_blank" rel="noreferrer noopener" href="https://alice333.ai/api/patreon">Patreon</a>
-                <a target="_blank" rel="noreferrer noopener" href="https://alice333.ai/api/youtube">YouTube</a>
-                <a target="_blank" rel="noreferrer noopener" href="https://alice333.ai/api/reddit">Reddit</a>
+            <p className="font-semibold mx-auto text-3xl text-semibold">Alice333.ai</p>
+            <div className="flex gap-3 p-3">
+                <Link link="https://alice333.ai/api/patreon" name="Patreon" />
+                <Link link="https://alice333.ai/api/youtube" name="YouTube" />
+                <Link link="https://alice333.ai/api/reddit" name="Reddit" />
             </div>
-            <div className="flex gap-1">
-                <a target="_blank" rel="noreferrer noopener" href="https://alice333.ai/api/discord">Discord</a>
-                <a target="_blank" rel="noreferrer noopener" href="https://alice333.ai/api/instagram">Instagram</a>
-                <a target="_blank" rel="noreferrer noopener" href="https://alice333.ai/api/alice333">Creator</a>
+            <div className="flex gap-3 p-3">
+                <Link link="https://alice333.ai/api/discord" name="Discord" />
+                <Link link="https://alice333.ai/api/instagram" name="Instagram" />
+                <Link link="https://alice333.ai/api/alice333" name="Creator" />
             </div>
 
             <button
@@ -31,6 +32,15 @@ const Socials: React.FC<Props> = ({ visible, setVis }: Props) => (
             </button>
         </div>
     </div>
+);
+
+interface LinkProps {
+    link: string,
+    name: string;
+}
+
+const Link: React.FC<LinkProps> = ({ link, name }: LinkProps) => (
+    <a target="_blank" rel="noreferrer noopener" className="md:text-3xl text-xl" href={link}>{name}</a>
 );
 
 export default Socials;
